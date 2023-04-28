cd0192ac6fbc517a5be8ba574ccbab49c5078c72
ealaimo18 <73393532+ealaimo18@users.noreply.github.com>
2023-04-28 10:56:43 -0400

    demo data script

---
+++ warning_ex.py
@@ -0,0 +1,25 @@
+import requests
+
+def main():
+    t = 25
+    p = 0
+    h = 40
+    pmt_2_5 = 23
+    aqi_2_5 = 18
+    pmt_10 = 72
+    aqi_10 = 72
+    url = 'http://161.253.92.155:5000/'
+    myobj = {"temperature": str(t), 
+                 "pressure": str(p),
+                 "humidity": str(h),
+                 "pm_2_5": str(pmt_2_5),
+                 "aqi_2_5": str(aqi_2_5),
+                 "pm_10": str(pmt_10),
+                 "aqi_10": str(aqi_10), 
+                 "device_id": 18}  #device id should be hard coded because it will never change with node
+    x = requests.post(url, json = myobj)
+    return
+
+if __name__ == "__main__":
+    main() 
+
