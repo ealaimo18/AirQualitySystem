b64c2a4f47810304683a305fb65841c0b7c5d005
Emmanuela Alaimo <73393532+ealaimo18@users.noreply.github.com>
2023-04-28 01:19:18 -0400

    we suck at git

--- main.py
+++ main.py
@@ -11,9 +11,43 @@ app.secret_key = "things"
 def home():
     temp = 0
     humid = 0
-    pm_inside = 0
-    pm_outside = 0
-    return render_template("index.html", temp=temp, humid=humid, pm_inside=pm_inside, pm_outside=pm_outside)
+    pm_2_5 = 0
+    pm_10 = 0
+    aqi_2_5 = 0
+    aqi_10 = 0
+    if request.method == 'POST':
+        content = request.get_json()
+        temp = content['temperature']
+        humid = content['humidity']
+        pm_2_5 = content['pm_2_5']
+        pm_10 = content['pm_10']
+        aqi_2_5 = content['aqi_2_5']
+        aqi_10 = content['aqi_10']
+    return render_template("index.html", temp=temp, humid=humid, pm_2_5 = pm_2_5, pm_10= pm_10,aqi_2_5 = aqi_2_5, aqi_10 = aqi_10 )
+
+#global variables
+phone = ""
+        # print(content['manue'])
+        # f = open("demofile3.txt", "a")
+        # f.write(content['manue'])
+        # f.close()
+
+def sendText(phone, message):
+    # Set environment variables for your credentials
+    # Read more at http://twil.io/secure
+    if request.method == 'POST':
+        phone = request.form['phone']
+    account_sid = "AC14a3f3132166d5d7afeb46621518c849"
+    auth_token = "2dfd6c6e1868ab9567e8e239757a73d9"
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+      body=message,
+      from_="+18776790836",
+      to= phone
+    #   to="+14846273900"
+    )
+    #print("text message sid", message.sid)
+    return redirect('/')
 
 @app.route('/registerDevice', methods=['GET', 'POST'])
 def registerDevice():
@@ -39,7 +73,7 @@ def textSignUp():
     auth_token = "2dfd6c6e1868ab9567e8e239757a73d9"
     client = Client(account_sid, auth_token)
     message = client.messages.create(
-      body="Test this works",
+      body="Congrats you've registered for texts!",
       from_="+18776790836",
       to= phone
     #   to="+14846273900"
@@ -47,32 +81,7 @@ def textSignUp():
     #print("text message sid", message.sid)
     return redirect('/')
 
-@app.route('/map', methods=['GET', 'POST'])
-def map():
 
-    #fetch data points
-    connection = sqlite3.connect("myDatabase.db")
-    connection.row_factory = sqlite3.Row
-    cursor = connection.cursor()
-    cursor.execute("SELECT * FROM DATA")
-    points = cursor.fetchall()
-    connection.commit()
 
-    list = []
 
-    # create list for a dictionary of each data point
-    for point in points:
-        data = {"temperature": point['temp'],
-                 "humidity": point['humid'],
-                 "pm_2_5":  point['pm_2_5'],
-                 "aqi_2_5":  point['aqi_2_5'],
-                 "pm_10":  point['pm_10'],
-                 "aqi_10":  point['aqi_10'],
-                 "lat":  point['lat'],
-                 "long":  point['long']}    
-        list.append(data)
-   
-    print(list)
-    return render_template("home.html")
-
-app.run(host='0.0.0.0', port=8080, debug=True)
+app.run(host='0.0.0.0', port=5000, debug=True)
